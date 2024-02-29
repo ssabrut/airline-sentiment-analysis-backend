06f91f59a267f6a9774804cd550b8e2739b4f266
ssabrut <michael.gunawan2002@gmail.com>
2024-02-29 20:58:01 +0700

    Add Embedding class

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from server.response import Response
+from server.model.embedding import Embedding
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
+    embedding = Embedding()
+    print(embedding.encode("Hello World"))
     return Response(message="Hello World", status=200)
